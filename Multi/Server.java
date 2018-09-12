1940ca627c8c50e9dab2172e652cb55c6aad741a
alan-lam <alanlam00@gmail.com>
2018-09-12 14:25:33 -0700

    username functionality

--- Multi/Server.java
+++ Multi/Server.java
@@ -5,7 +5,6 @@ import java.util.*;
 
 public class Server {
    static Vector<ClientHandler> ar = new Vector<>();
-   static int i = 1;
 
    public static void main (String[] args) throws IOException {
       ServerSocket serverSocket = new ServerSocket(5056);
@@ -17,16 +16,15 @@ public class Server {
          try {
             System.out.println("Waiting for a client");
             clientSocket = serverSocket.accept();
-            System.out.println("Client " + i + " accepted");
+            System.out.println("Client accepted");
 
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             PrintWriter out= new PrintWriter(clientSocket.getOutputStream(), true);
 
-            ClientHandler c = new ClientHandler(clientSocket, "client " + i, in, out);
+            ClientHandler c = new ClientHandler(clientSocket, in, out);
             Thread t = new Thread(c);
             ar.add(c);
             t.start();
-            i++;
          }
          catch (Exception e) {
             clientSocket.close();
@@ -43,10 +41,13 @@ class ClientHandler implements Runnable {
    final Socket s;
    boolean isLoggedIn = true;
 	
-   public ClientHandler (Socket s, String name, BufferedReader in, PrintWriter out) {
+   public ClientHandler (Socket s, BufferedReader in, PrintWriter out) {
       this.s = s;
       this.in = in;
       this.out = out;
+   }
+
+   public void setName(String name) {
       this.name = name;
    }
 
@@ -54,7 +55,14 @@ class ClientHandler implements Runnable {
    public void run() {
       String received;
 
-      out.println("Hello. You are " + this.name);
+      out.println("Hello. Enter your name: ");
+      try {
+         this.setName(in.readLine());
+      }
+      catch (Exception e) {
+         e.printStackTrace();
+      }
+      out.println("Great! Your name is " + this.name);
 
       while (true) {
          try {
@@ -68,9 +76,9 @@ class ClientHandler implements Runnable {
                break;
             }
 
-            StringTokenizer st = new StringTokenizer(received, "#");
-            String toSend = st.nextToken();
+            StringTokenizer st = new StringTokenizer(received, "@");
             String recipient = st.nextToken();
+            String toSend = st.nextToken();
 
             for (ClientHandler mc : Server.ar) {
                if (mc.name.equals(recipient) && mc.isLoggedIn == true) {
